67ed99522792b2f8e083989a04327b03bd5409d2
Lucas Ribeiro Magalhaes <lucas.magalhaes@murabei.com>
2026-08-04 11:38:41 -0300

    update build process with Poetry; added CI workflow for automated publishing.

--- build.sh
+++ build.sh
@@ -1,7 +1,23 @@
-source VERSION
-sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.py > setup.py
+# Update version path
+VERSION_FILE="VERSION"
+current_version=$(grep -E "^VERSION=" "$VERSION_FILE" | cut -d'=' -f2)
+IFS='.' read -r major minor patch <<< "$current_version"
+patch=$((patch + 1))
+new_version="$major.$minor.$patch"
+sed -i "s/^VERSION=.*/VERSION=$new_version/" "$VERSION_FILE"
+echo "Updated version to $new_version"
 
-python3 setup.py build sdist bdist_wheel
+source VERSION
+# Detecta branch atual e adiciona -beta se não for main/master
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$CURRENT_BRANCH" != "main" && "$CURRENT_BRANCH" != "master" ]]; then
+  VERSION="${VERSION}-b.0"
+fi
+
+sed -e 's#{VERSION}#'"${VERSION}"'#g' pyproject_template.toml > pyproject.toml
+
+rm -R build/
+poetry build
 
 git add --all
 git commit -m "Building a new version ${VERSION}"
